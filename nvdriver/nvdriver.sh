9a7a03d3e73a514ebd30c7fed11579962f2e6ea2
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-05-30 22:58:42 +0300

    Update nvdriver.sh

--- nvdriver/nvdriver.sh
+++ nvdriver/nvdriver.sh
@@ -7,4 +7,4 @@ sudo dnf --enablerepo=rpmfusion-nonfree-rawhide install akmod-nvidia-open xorg-x
 sudo cp nvidia-kms.conf /etc/modprobe.d/
 #sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power
 #sudo dnf install xorg-x11-drv-nvidia-libs.i686
-sudo cp nvidia-kms.conf /etc/modprobe.d/
+#sudo cp nvidia-kms.conf /etc/modprobe.d/
