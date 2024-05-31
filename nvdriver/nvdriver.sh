dad10aa5d3e8962525d706e20e57e0815c37b607
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2024-06-01 01:46:16 +0300

    Update nvdriver.sh

--- nvdriver/nvdriver.sh
+++ nvdriver/nvdriver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #nvdriver
-sudo dnf copr enable kwizart/nvidia-driver-rawhide -y
-sudo dnf install rpmfusion-nonfree-release-rawhide -y
-sudo dnf --enablerepo=rpmfusion-nonfree-rawhide install akmod-nvidia-open xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda --nogpgcheck
+#sudo dnf copr enable kwizart/nvidia-driver-rawhide -y
+#sudo dnf install rpmfusion-nonfree-release-rawhide -y
+#sudo dnf --enablerepo=rpmfusion-nonfree-rawhide install akmod-nvidia-open xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda --nogpgcheck
+#nvd
+sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power
+sudo dnf install xorg-x11-drv-nvidia-libs.i686
 sudo cp nvidia-kms.conf /etc/modprobe.d/
-#sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power
-#sudo dnf install xorg-x11-drv-nvidia-libs.i686
-#sudo cp nvidia-kms.conf /etc/modprobe.d/
